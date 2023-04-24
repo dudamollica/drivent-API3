7bd4583a1b25443088ce3fe6712fe75c08c65cba
dudamollica <dudarenno@yahoo.com.br>
2023-04-24 12:25:10 -0300

    Feat: Add Not found error at route '/hotels'

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,22 +1,24 @@
 import { notFoundError } from '@/errors';
-import hotelsRepository from "@/repositories/hotels-repository/index"
+import hotelsRepository from '@/repositories/hotels-repository/index';
 import ticketsRepository from '@/repositories/tickets-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import { Hotel } from '@prisma/client';
 import httpStatus from 'http-status';
 
-async function listAllHotels(userId: number) : Promise<Hotel[]>{
-    const enrollment = await enrollmentRepository.findByUserId(userId)
-    if(!enrollment) throw notFoundError()
+async function listAllHotels(userId: number): Promise<Hotel[]> {
+  const enrollment = await enrollmentRepository.findByUserId(userId);
+  if (!enrollment) throw notFoundError();
 
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id)
-    if(!ticket) throw notFoundError()
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
 
-    if(ticket.status != "PAID") throw httpStatus.PAYMENT_REQUIRED
-    if(ticket.TicketType.isRemote == true) throw httpStatus.PAYMENT_REQUIRED
-    if(ticket.TicketType.includesHotel == false) throw httpStatus.PAYMENT_REQUIRED
+  if (ticket.status != 'PAID') throw httpStatus.PAYMENT_REQUIRED;
+  if (ticket.TicketType.isRemote == true) throw httpStatus.PAYMENT_REQUIRED;
+  if (ticket.TicketType.includesHotel == false) throw httpStatus.PAYMENT_REQUIRED;
 
-    return await hotelsRepository.findAll()
+  const hotels = await hotelsRepository.findAll();
+  if (!hotels) throw notFoundError();
+  return hotels;
 }
 
 export default {
